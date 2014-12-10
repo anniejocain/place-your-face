120e8729c0fbffe3026b14198806234df88f3bd6
Matt Phillips <mphillips@awesome.law.harvard.edu>
2014-12-10 13:15:32 -0500

    Added basic db insertion

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+        'servername' => 'your mysql server',
+        'username' => 'your mysql username',
+        'password' => 'your mysql pw',
+        'dbname' => 'your mysql db name',
+);
+?>
